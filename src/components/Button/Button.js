776ddbefed9c60a212aaab94fe556e65319f590a
mcskypl <mcskypl@gmail.com>
2019-05-12 15:01:57 +0200

    Add styled-components props no.2.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,22 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  width: ${({ secondary }) => (secondary ? '110px' : '220px')};
-  height: ${({ secondary }) => (secondary ? '30px' : '47px')};
-  background-color: ${({ secondary }) => (secondary ? 'gray' : 'yellow')};
+  width: 220px;
+  height: 47px;
+  background-color: yellow;
   border: none;
   border-radius: 50px;
   font-weight: 900;
   text-transform: uppercase;
   cursor: pointer;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      width: 110px;
+      height: 30px;
+      background-color: lightgray;
+    `}
 `;
 
 export default Button;
