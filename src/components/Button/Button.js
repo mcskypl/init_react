680e8d3e4d3b278bd06b63e0e35aa6d8e8749c48
mcskypl <mcskypl@gmail.com>
2019-05-12 14:24:46 +0200

    Add first styed-component.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: yellow;
+  padding: 0.8em;
+`;
+
+export default Button;
---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Button from '../../components/Button/Button';
+
+const Root = () => (
+  <>
+    <h1>Hello React</h1>
+    <Button>Hello</Button>
+  </>
+);
+
+export default Root;
